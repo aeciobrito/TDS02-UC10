75409dbc72b5e4e3a330c341233d5c7f9e66abed
AECIO MENDES BRITO <aeciobrito@gmail.com>
2026-04-08 20:55:26 -0300

    Cadastro de pedidos na PedidoService

--- 01/ControleEstoque/ControleEstoque.API/Models/Pedido.cs
+++ 01/ControleEstoque/ControleEstoque.API/Models/Pedido.cs
@@ -13,6 +13,14 @@ namespace ControleEstoque.API.Models
         [Required, StringLength(20)]
         public string Status { get; set; } // aberto, fechado, supspenso...
 
+        [ForeignKey("Cliente")] // Cliente que fez o pedido
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        [ForeignKey("Caixa")] // Caixa que fecha o pedido
+        public int? CaixaId { get; set; }
+        public Caixa Caixa { get; set; }
+
         public ICollection<ItemPedido> Itens { get; set; } = new List<ItemPedido>();        
     }
 }
